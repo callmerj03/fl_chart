51d0e0334d11a916b752f1277fb13178cf2c65dd
“callmerj03” <jinwalarohan03@gmail.com>
2024-08-07 15:24:17 +0530

    - update to 3.22.2

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -44,10 +44,8 @@ class Utils {
   }
 
   Offset calculateRotationOffset(Size size, double degree) {
-    final rotatedHeight = (size.width * math.sin(radians(degree))).abs() +
-        (size.height * cos(radians(degree))).abs();
-    final rotatedWidth = (size.width * cos(radians(degree))).abs() +
-        (size.height * sin(radians(degree))).abs();
+    final rotatedHeight = (size.width * math.sin(radians(degree))).abs() + (size.height * cos(radians(degree))).abs();
+    final rotatedWidth = (size.width * cos(radians(degree))).abs() + (size.height * sin(radians(degree))).abs();
     return Offset(
       (size.width - rotatedWidth) / 2,
       (size.height - rotatedHeight) / 2,
@@ -64,32 +62,28 @@ class Utils {
     }
 
     Radius topLeft;
-    if (borderRadius.topLeft.x > width / 2 ||
-        borderRadius.topLeft.y > width / 2) {
+    if (borderRadius.topLeft.x > width / 2 || borderRadius.topLeft.y > width / 2) {
       topLeft = Radius.circular(width / 2);
     } else {
       topLeft = borderRadius.topLeft;
     }
 
     Radius topRight;
-    if (borderRadius.topRight.x > width / 2 ||
-        borderRadius.topRight.y > width / 2) {
+    if (borderRadius.topRight.x > width / 2 || borderRadius.topRight.y > width / 2) {
       topRight = Radius.circular(width / 2);
     } else {
       topRight = borderRadius.topRight;
     }
 
     Radius bottomLeft;
-    if (borderRadius.bottomLeft.x > width / 2 ||
-        borderRadius.bottomLeft.y > width / 2) {
+    if (borderRadius.bottomLeft.x > width / 2 || borderRadius.bottomLeft.y > width / 2) {
       bottomLeft = Radius.circular(width / 2);
     } else {
       bottomLeft = borderRadius.bottomLeft;
     }
 
     Radius bottomRight;
-    if (borderRadius.bottomRight.x > width / 2 ||
-        borderRadius.bottomRight.y > width / 2) {
+    if (borderRadius.bottomRight.x > width / 2 || borderRadius.bottomRight.y > width / 2) {
       bottomRight = Radius.circular(width / 2);
     } else {
       bottomRight = borderRadius.bottomRight;
@@ -138,8 +132,7 @@ class Utils {
     if (diffInAxis == 0) {
       return 1;
     }
-    final accurateInterval =
-        diffInAxis == 0 ? axisViewSize : diffInAxis / allowedCount;
+    final accurateInterval = diffInAxis == 0 ? axisViewSize : diffInAxis / allowedCount;
     if (allowedCount <= 2) {
       return accurateInterval;
     }
@@ -262,9 +255,8 @@ class Utils {
     if (providedStyle == null || providedStyle.inherit) {
       effectiveTextStyle = defaultTextStyle.style.merge(providedStyle);
     }
-    if (MediaQuery.boldTextOverride(context)) {
-      effectiveTextStyle = effectiveTextStyle!
-          .merge(const TextStyle(fontWeight: FontWeight.bold));
+    if (MediaQuery.boldTextOf(context)) {
+      effectiveTextStyle = effectiveTextStyle!.merge(const TextStyle(fontWeight: FontWeight.bold));
     }
     return effectiveTextStyle!;
   }
